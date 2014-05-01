3b7b75daa6efa3b00f0e529517e0cb4405f229ce
Stephen Kell <srk31@cl.cam.ac.uk>
2014-05-01 15:43:38 +0100

    checkout: support git as well as hggit.

--- contrib/checkout.sh
+++ contrib/checkout.sh
@@ -21,12 +21,17 @@ gen_makefile () {
     echo "include Makefile"
 }
 
-([ -d libcxxfileno ] || (hg clone git+https://github.com/stephenrkell/libcxxfileno.git libcxxfileno)) && ${MAKE} -C libcxxfileno && \
-([ -d libsrk31cxx  ] || hg clone git+https://github.com/stephenrkell/libsrk31cxx.git libsrk31cxx) && gen_makefile "$( readlink -f libcxxfileno )" > libsrk31cxx/src/makefile && ${MAKE} -C libsrk31cxx && \
-([ -d libdwarfpp ] || hg clone git+https://github.com/stephenrkell/libdwarfpp.git libdwarfpp) && \
+checkout () {
+    hg clone git+https://github.com/stephenrkell/$1.git $1 || \
+    git clone https://github.com/stephenrkell/$1.git $1
+}
+
+([ -d libcxxfileno ] || checkout libcxxfileno) && ${MAKE} -C libcxxfileno && \
+([ -d libsrk31cxx  ] || checkout libsrk31cxx) && gen_makefile "$( readlink -f libcxxfileno )" > libsrk31cxx/src/makefile && ${MAKE} -C libsrk31cxx && \
+([ -d libdwarfpp ] || checkout libdwarfpp) && \
   gen_makefile "$( readlink -f libcxxfileno )" "$( readlink -f libsrk31cxx )"> libdwarfpp/src/makefile && \
   gen_makefile "$( readlink -f libcxxfileno )" "$( readlink -f libsrk31cxx )"> libdwarfpp/examples/makefile && \
    ${MAKE} -C libdwarfpp && \
-([ -d libcxxgen ] || hg clone git+https://github.com/stephenrkell/libcxxgen.git libcxxgen) && gen_makefile "$( readlink -f libcxxfileno )" "$( readlink -f libsrk31cxx )" "$( readlink -f libdwarfpp )"> libcxxgen/src/makefile && ${MAKE} -C libcxxgen && \
+([ -d libcxxgen ] || checkout libcxxgen) && gen_makefile "$( readlink -f libcxxfileno )" "$( readlink -f libsrk31cxx )" "$( readlink -f libdwarfpp )"> libcxxgen/src/makefile && ${MAKE} -C libcxxgen && \
 gen_makefile "$( readlink -f libcxxfileno )" "$( readlink -f libsrk31cxx )" "$( readlink -f libdwarfpp )" "$( readlink -f libcxxgen )" \
 > $(dirname $0)/../src/makefile
