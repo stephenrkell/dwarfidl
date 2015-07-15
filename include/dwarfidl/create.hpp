4fa97442d544228e8a96db8aad8caa0f4134e0d0
Jonathan French <me@jonathanfrench.net>
2015-07-15 15:01:51 +0100

    multi pass approach to allow forward references of identifiers

--- include/dwarfidl/create.hpp
+++ include/dwarfidl/create.hpp
@@ -4,6 +4,8 @@
 
 #include "dwarfidl/lang.hpp"
 #include <dwarfpp/lib.hpp>
+#include <vector>
+#include <map>
 
 namespace dwarfidl
 {
@@ -11,14 +13,16 @@ namespace dwarfidl
 	using namespace dwarf::core;
 
 	iterator_base create_dies(antlr::tree::Tree *ast);
-	iterator_base create_dies(const iterator_base& parent, antlr::tree::Tree *ast);
-	iterator_base create_dies(const iterator_base& parent, const string& some_dwarfidl);
-	iterator_base create_one_die(const iterator_base& parent, antlr::tree::Tree *ast, 
-		const std::map<antlr::tree::Tree *, iterator_base>& nested
-		 = std::map<antlr::tree::Tree *, iterator_base>()
-	);
-	iterator_base create_one_die_with_children(const iterator_base& parent, antlr::tree::Tree *ast);
-	iterator_base find_or_create_die(const iterator_base& parent, antlr::tree::Tree *ast);
+	iterator_base create_dies(iterator_base& parent, antlr::tree::Tree *ast);
+	iterator_base create_dies(iterator_base& parent, const string& some_dwarfidl);
+	iterator_base create_one_die(iterator_base& parent, antlr::tree::Tree *ast, 
+								 std::vector<std::pair<iterator_base&, antlr::tree::Tree*> > &postpone,
+								 const std::map<antlr::tree::Tree *, iterator_base>& nested
+								 = std::map<antlr::tree::Tree *, iterator_base>());
+	 iterator_base create_one_die_with_children(iterator_base& parent, antlr::tree::Tree *ast,
+												std::vector<std::pair<iterator_base&, antlr::tree::Tree*> > &postpone);
+	 iterator_base find_or_create_die(iterator_base& parent, antlr::tree::Tree *ast,
+									  std::vector<std::pair<iterator_base&, antlr::tree::Tree*> > &postpone);
 
 	encap::attribute_value make_attribute_value(antlr::tree::Tree *d, 
 		const iterator_base& context, 
