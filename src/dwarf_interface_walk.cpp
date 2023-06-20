8d3e4b19586a3bbe1164d5a2c4abcd885e0b9d06
Stephen Kell <srk31@srcf.ucam.org>
2023-06-20 22:26:43 +0100

    dwarf_interface_walk.cpp: cleanups

--- src/dwarf_interface_walk.cpp
+++ src/dwarf_interface_walk.cpp
@@ -1,7 +1,3 @@
-/* This is a simple dwarfpp program which generates a C file
- * recording data on a uniqued set of data types  allocated in a given executable.
- */
- 
 #include <fstream>
 #include <sstream>
 #include <map>
@@ -38,29 +34,6 @@ using std::unordered_set;
 using namespace dwarf;
 using namespace dwarf::core;
 
-// //using boost::filesystem::path;
-// using dwarf::core::root_die;
-// using dwarf::core::iterator_base;
-// using dwarf::core::iterator_df;
-// using dwarf::core::iterator_sibs;
-// using dwarf::core::type_set;
-// using dwarf::core::type_die;
-// using dwarf::core::subprogram_die;
-// using dwarf::core::formal_parameter_die;
-// using dwarf::core::compile_unit_die;
-// using dwarf::core::typedef_die;
-// using dwarf::core::member_die;
-// using dwarf::core::with_data_members_die;
-// using dwarf::core::variable_die;
-// using dwarf::core::program_element_die;
-// using dwarf::core::with_dynamic_location_die;
-// using dwarf::core::address_holding_type_die;
-// using dwarf::core::pointer_type_die;
-// using dwarf::core::union_type_die;
-// using dwarf::core::array_type_die;
-// using dwarf::core::type_chain_die;
-// using dwarf::core::base_type_die;
-// using dwarf::core::qualified_type_die;
 using dwarf::spec::opt;
 
 using dwarf::lib::Dwarf_Off;
@@ -78,166 +51,94 @@ using std::function;
 
 namespace dwarf { namespace tool {
 
-
-		  
-// 		  template <typename Item>
-// class mru_list
-// {
-//   typedef multi_index_container<
-//     Item,
-//     indexed_by<
-//       sequenced<>,
-//       hashed_unique<identity<Item> >
-//     >
-//   > item_list;
-
-// public:
-//   typedef Item                         item_type;
-//   typedef typename item_list::iterator iterator;
-
-//   mru_list(std::size_t max_num_items_):max_num_items(max_num_items_){}
-
-//   void insert(const item_type& item)
-//   {
-//     std::pair<iterator,bool> p=il.push_front(item);
-
-//     if(!p.second){                     /* duplicate item */
-//       il.relocate(il.begin(),p.first); /* put in front */
-//     }
-//     else if(il.size()>max_num_items){  /* keep the length <= max_num_items */
-//       il.pop_back();
-//     }
-//   }
-
-//   iterator begin(){return il.begin();}
-//   iterator end(){return il.end();}
-
-// private:
-//   item_list   il;
-//   std::size_t max_num_items;
-
-
-// };
-
-
-
-		  
-		  void my_walk_type(iterator_df<type_die> t, iterator_df<program_element_die> reason, 
-							const std::function<bool(iterator_df<type_die>, iterator_df<program_element_die>)>& pre_f, 
-							const std::function<void(iterator_df<type_die>, iterator_df<program_element_die>)>& post_f
-							= std::function<void(core::iterator_df<core::type_die>, core::iterator_df<core::program_element_die>)>(),
-							const type_set& currently_walking = type_set()
-							// const mru_list<type_die> &cache = mru_list<type_die>()
-			   )
-		  {
+void my_walk_type(iterator_df<type_die> t, iterator_df<program_element_die> reason, 
+					const std::function<bool(iterator_df<type_die>, iterator_df<program_element_die>)>& pre_f, 
+					const std::function<void(iterator_df<type_die>, iterator_df<program_element_die>)>& post_f
+					= std::function<void(core::iterator_df<core::type_die>, core::iterator_df<core::program_element_die>)>(),
+					const type_set& currently_walking = type_set()
+					// const mru_list<type_die> &cache = mru_list<type_die>()
+	   )
+{
 //			   if (cache.find(t) != cache.end()) return;
-			if (currently_walking.find(t) != currently_walking.end()) return; // "grey node"
-			
-			bool continue_recursing;
-			if (pre_f) continue_recursing = pre_f(t, reason); // i.e. we do walk "void"
-			else continue_recursing = true;
-			
-			type_set next_currently_walking = currently_walking; 
-			next_currently_walking.insert(t);
-			
-			if (continue_recursing)
-			{
+	if (currently_walking.find(t) != currently_walking.end()) return; // "grey node"
 
-				if (!t) { /* void case; just post-visit */ }
-				else if (t.is_a<type_chain_die>()) // unary case -- includes typedefs, arrays, pointer/reference, ...
-				{
-					// recursively walk the chain's target
-					 my_walk_type(t.as_a<type_chain_die>()->find_type(), t, pre_f, post_f, next_currently_walking);
-				}
-				else if (t.is_a<with_data_members_die>()) 
-				{
-					// recursively walk all members
-					auto member_children = t.as_a<with_data_members_die>().children().subseq_of<member_die>();
-					for (auto i_child = member_children.first;
-						i_child != member_children.second; ++i_child)
-					{
-						my_walk_type(i_child->find_type(), i_child, pre_f, post_f , next_currently_walking);
-					}
-					// visit all inheritances
-					auto inheritance_children = t.as_a<with_data_members_die>().children().subseq_of<inheritance_die>();
-					for (auto i_child = inheritance_children.first;
-						i_child != inheritance_children.second; ++i_child)
-					{
-						 my_walk_type(i_child->find_type(), i_child, pre_f, post_f , next_currently_walking);
-					}
+	bool continue_recursing;
+	if (pre_f) continue_recursing = pre_f(t, reason); // i.e. we do walk "void"
+	else continue_recursing = true;
 
-					/*auto children_with_type = t.as_a<with_data_members_die>().children().subseq_of<with_type_describing_layout_die>();
-					for (auto i_child = children_with_type.first; i_child != children_with_type.second; i_child++) {
-						 my_walk_type(i_child->find_type(), i_child, pre_f, post_f , next_currently_walking);
-						 }*/
-					}
-				else if (t.is_a<subrange_type_die>())
-				{
-					// visit the base type
-					auto explicit_t = t.as_a<subrange_type_die>()->find_type();
-					// HACK: assume this is the same as for enums
-					my_walk_type(explicit_t ? explicit_t : t.enclosing_cu()->implicit_enum_base_type(), t, pre_f, post_f, next_currently_walking);
-				}
-				else if (t.is_a<enumeration_type_die>())
-				{
-					// visit the base type -- HACK: assume subrange base is same as enum's
-					auto explicit_t = t.as_a<enumeration_type_die>()->find_type();
-					my_walk_type(explicit_t ? explicit_t : t.enclosing_cu()->implicit_enum_base_type(), t, pre_f, post_f, next_currently_walking);
-				}
-				else if (t.is_a<type_describing_subprogram_die>())
-				{
-					auto sub_t = t.as_a<type_describing_subprogram_die>();
-					my_walk_type(sub_t->find_type(), sub_t, pre_f, post_f);
-					auto fps = sub_t.children().subseq_of<formal_parameter_die>();
-					for (auto i_fp = fps.first; i_fp != fps.second; ++i_fp)
-					{
-						my_walk_type(i_fp->find_type(), i_fp, pre_f, post_f, next_currently_walking);
-					}
-					/*auto vars = sub_t.children().subseq_of<variable_die>();
-					for (auto i_fp = vars.first; i_fp != vars.second; ++i_fp)
-					{
-						my_walk_type(i_fp->find_type(), i_fp, pre_f, post_f, next_currently_walking);
-						}*/
-				
-				}
-				else
-				{
-					// what are our nullary cases?
-					assert(t.is_a<base_type_die>() || t.is_a<unspecified_type_die>());
+	type_set next_currently_walking = currently_walking; 
+	next_currently_walking.insert(t);
 
-/*					function<void(iterator_df<type_die>,
-								  iterator_df<program_element_die>)> traverse_children = [&traverse_children,
-																						  &pre_f,
-																						  &post_f,
-																						  &next_currently_walking](iterator_df<type_die> t,
-																										 iterator_df<program_element_die> reason){
-						 auto children = t.children();
-						 for (auto iter = children.first; iter != children.second; iter++) {
-							  auto child_type = iter.as_a<type_die>();
-							  auto child_with_type = iter.as_a<with_type_describing_layout_die>();
-							  auto child_returning_type = iter.as_a<type_describing_subprogram_die>();
-							  if (child_type) {
-								   my_walk_type(child_type, t, pre_f, post_f, next_currently_walking);
-							  } else if (child_with_type) {
-								   my_walk_type(child_with_type->get_type(), t, pre_f, post_f, next_currently_walking);
-							  } else if (child_returning_type) {
-								   my_walk_type(child_returning_type->get_type(), t, pre_f, post_f, next_currently_walking);
-							  } else {
-								   traverse_children(iter, t);
-							  }
-						 }
-					};
+	if (continue_recursing)
+	{
 
-					traverse_children(t, reason);*/
-					
-				}
-			} // end if continue_recursing
-
-			if (post_f) post_f(t, reason);
+		if (!t) { /* void case; just post-visit */ }
+		else if (t.is_a<type_chain_die>()) // unary case -- includes typedefs, arrays, pointer/reference, ...
+		{
+			// recursively walk the chain's target
+			my_walk_type(t.as_a<type_chain_die>()->find_type(), t, pre_f, post_f, next_currently_walking);
 		}
+		else if (t.is_a<with_data_members_die>()) 
+		{
+			// recursively walk all members
+			auto member_children = t.as_a<with_data_members_die>().children().subseq_of<member_die>();
+			for (auto i_child = member_children.first;
+				i_child != member_children.second; ++i_child)
+			{
+				my_walk_type(i_child->find_type(), i_child, pre_f, post_f , next_currently_walking);
+			}
+			// visit all inheritances
+			auto inheritance_children = t.as_a<with_data_members_die>().children().subseq_of<inheritance_die>();
+			for (auto i_child = inheritance_children.first;
+				i_child != inheritance_children.second; ++i_child)
+			{
+				 my_walk_type(i_child->find_type(), i_child, pre_f, post_f , next_currently_walking);
+			}
 
+			/*auto children_with_type = t.as_a<with_data_members_die>().children().subseq_of<with_type_describing_layout_die>();
+			for (auto i_child = children_with_type.first; i_child != children_with_type.second; i_child++) {
+				 my_walk_type(i_child->find_type(), i_child, pre_f, post_f , next_currently_walking);
+				 }*/
+			}
+		else if (t.is_a<subrange_type_die>())
+		{
+			// visit the base type
+			auto explicit_t = t.as_a<subrange_type_die>()->find_type();
+			// HACK: assume this is the same as for enums
+			my_walk_type(explicit_t ? explicit_t : t.enclosing_cu()->implicit_enum_base_type(), t, pre_f, post_f, next_currently_walking);
+		}
+		else if (t.is_a<enumeration_type_die>())
+		{
+			// visit the base type -- HACK: assume subrange base is same as enum's
+			auto explicit_t = t.as_a<enumeration_type_die>()->find_type();
+			my_walk_type(explicit_t ? explicit_t : t.enclosing_cu()->implicit_enum_base_type(), t, pre_f, post_f, next_currently_walking);
+		}
+		else if (t.is_a<type_describing_subprogram_die>())
+		{
+			auto sub_t = t.as_a<type_describing_subprogram_die>();
+			my_walk_type(sub_t->find_type(), sub_t, pre_f, post_f);
+			auto fps = sub_t.children().subseq_of<formal_parameter_die>();
+			for (auto i_fp = fps.first; i_fp != fps.second; ++i_fp)
+			{
+				my_walk_type(i_fp->find_type(), i_fp, pre_f, post_f, next_currently_walking);
+			}
+			/*auto vars = sub_t.children().subseq_of<variable_die>();
+			for (auto i_fp = vars.first; i_fp != vars.second; ++i_fp)
+			{
+				my_walk_type(i_fp->find_type(), i_fp, pre_f, post_f, next_currently_walking);
+				}*/
 
-		  
+		}
+		else
+		{
+			// what are our nullary cases?
+			assert(t.is_a<base_type_die>() || t.is_a<unspecified_type_die>());
+
+		}
+	} // end if continue_recursing
+
+	if (post_f) post_f(t, reason);
+}
 
 void 
 gather_interface_dies(root_die& root, 
@@ -254,7 +155,7 @@ gather_interface_dies(root_die& root,
 	/* FIXME: it needn't be just grandchildren. */
 	for (auto i_d = std::move(toplevel_seq.first); i_d != toplevel_seq.second; ++i_d)
 	{
-		std::cerr << "\rAt " << i_d.summary();
+		std::cerr << "\r" << i_d.summary(); // for debugging
 		if (pred(i_d))
 		{
 			std::cerr << std::endl;
